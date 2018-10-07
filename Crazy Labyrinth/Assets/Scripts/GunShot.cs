625237cd206d3cc69bac1fadc056210988dbb664
Jake <jp882889@wcupa.edu>
2018-10-06 23:49:45 -0400

    This is the beginning of the gun code. Right now this was just to make sure I could get bullets to shoot from each gun in each direction. More will come.

---
+++ Crazy Labyrinth/Assets/Scripts/GunShot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShot : MonoBehaviour {
+    public GameObject bulletPrefab;
+
+    // Use this for initialization
+    void Start () {
+
+        // Calls Fire every second
+        InvokeRepeating("Fire", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void Fire()
+    {
+        // Spawns the bullet
+        var bullet = (GameObject)Instantiate(
+        bulletPrefab,
+        transform.position,
+        transform.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 6;
+
+        // Destroys the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
