9c9914b6fd8335f6f38209b41a9adfe9b327d243
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-06 20:56:11 -0400

    Ball Movement
    This only affects lvl 1 as of right now.
    *Rotated Game Map
    *Added Sphere Object
    *Added Script for Player Input
    *Added Script template for Camera Movement

---
+++ Crazy Labyrinth/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>(); //Gets access Rigidbody
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //This code is used so that player input can move the ball
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //This code is for when bullets hit the sphere
+    }
+}
