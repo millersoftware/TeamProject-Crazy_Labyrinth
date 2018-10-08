839be254669764a15e06d953cd2505a41c289e0c
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-07 20:55:00 -0400

    TimeTracker
    *Added Dictonary that tracks level as the key and updates value with time, until level is finished.
    *Added template for Score Script that will be used for buttons eventually

---
+++ Crazy Labyrinth/Assets/Scripts/TimeTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeTracker : MonoBehaviour {
+    public static Dictionary<string, float> scoreDic = new Dictionary<string, float>();
+    public float timeOf = 0.0f;
+    public string sceneName;
+    // Use this for initialization
+    void Start () {
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        scoreDic.Add(sceneName, timeOf);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeOf = Time.timeSinceLevelLoad;//Tracks time since level has been loaded
+        scoreDic[sceneName] = timeOf;
+    }
+}
