592e0dab1a01de8fa464ff6390007594568c33fe
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-09 10:05:16 -0400

    ScoreScreen Fix
    *Made it so score screen is no longer dependent on knowing level in advance.

--- Crazy Labyrinth/Assets/Scripts/ChangeScene.cs
+++ Crazy Labyrinth/Assets/Scripts/ChangeScene.cs
@@ -5,10 +5,20 @@ using UnityEngine.SceneManagement;
 /*
 * This class is only used for buttons to change scenes
 */
-public class ChangeScene : MonoBehaviour {
 
+public class ChangeScene : MonoBehaviour {
+    public static int current = 1;
     public void Change(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+    public void ChangeToNextLevel()
+    {
+        current += 1;
+        SceneManager.LoadScene(current);
+    }
+    public void KeepLevel()
+    {
+        SceneManager.LoadScene(current);
     }
 }
