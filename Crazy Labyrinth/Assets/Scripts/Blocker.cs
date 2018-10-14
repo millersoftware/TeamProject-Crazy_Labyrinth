283c47f20e1e0e28fc3ca38ef940d056880f96e8
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-14 00:51:28 -0400

    Jacobs File

---
+++ Crazy Labyrinth/Assets/Scripts/Blocker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Blocker"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
